46ef7e6254763f6ff6bf62a4ffa829d2e8c3f02d
tekk1337 <58187035+tekk1337@users.noreply.github.com>
2021-02-05 16:03:29 -0600

    Add files via upload

--- newinfo.ps1
+++ newinfo.ps1
@@ -8,8 +8,7 @@
     [switch]$ciphercheck,
     [switch]$installedsoftware,
     [switch]$patchcheck,
-    [switch]$armorservices,
-    [switch]$showsubagents
+    [switch]$showarmorservices
     )
 
 Begin {
@@ -62,7 +61,7 @@ Begin {
         }
     function PendingReboot
         {
-        Write-Host "Pending Reboot" -ForegroundColor Yellow
+        Write-Output "Pending Reboot" 
         if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { return $true }
         if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { return $true }
         if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { return $true }
@@ -78,7 +77,7 @@ Begin {
         }
     Function Protocolcheck
         {
-        Write-Host "Protocols" -ForegroundColor Yellow
+        Write-Output "Protocols" w
         $ErrorActionPreference = "SilentlyContinue"
         $OScheck = Get-CimInstance Win32_OperatingSystem | select -ExpandProperty Caption | Select-String -Pattern '20\d\d' | ForEach-Object { $_.Matches.Value }
         $SSL2 = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server' | select -ExpandProperty Enabled
@@ -86,39 +85,41 @@ Begin {
         $tls10 = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' | select -ExpandProperty Enabled
         $tls11 = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' | select -ExpandProperty Enabled
         $tls12 = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' | select -ExpandProperty Enabled
-        If ($tls10 -eq 0 -or $tls10 -eq $null){Write-Host "TLS 1.0 is Disabled"}Else{Write-Host "TLS 1.0 is Enabled"}
+        If ($tls10 -eq 0 -or $tls10 -eq $null){Write-Output "TLS 1.0 is Disabled"}Else{Write-Output "TLS 1.0 is Enabled"}
         If ($OScheck -gt 2012)
         {
-        If ($tls11 -eq 0) {Write-Host "TLS 1.1 is Disabled"}Else{Write-Host "TLS 1.1 is Enabled"}
-        If ($tls12 -eq 0) {Write-Host "TLS 1.2 is Disabled"}Else{Write-Host "TLS 1.2 is Enabled"}
+        If ($tls11 -eq 0) {Write-Output "TLS 1.1 is Disabled"}Else{Write-Output "TLS 1.1 is Enabled"}
+        If ($tls12 -eq 0) {Write-Output "TLS 1.2 is Disabled"}Else{Write-Output "TLS 1.2 is Enabled"}
         }Else{
-        If ($tls11 -eq 0 -eq $null) {Write-Host "TLS 1.1 is Disabled"}Else{Write-Host "TLS 1.1 is Enabled"}
-        If ($tls12 -eq 0 -eq $null) {Write-Host "TLS 1.2 is Disabled"}Else{Write-Host "TLS 1.2 is Enabled"}
+        If ($tls11 -eq 0 -eq $null) {Write-Output "TLS 1.1 is Disabled"}Else{Write-Output "TLS 1.1 is Enabled"}
+        If ($tls12 -eq 0 -eq $null) {Write-Output "TLS 1.2 is Disabled"}Else{Write-Output "TLS 1.2 is Enabled"}
         }}
     function CipherCheck {
-        Write-Host "Ciphers" -ForegroundColor Yellow
+        Write-Output "Ciphers"  
         $ciphercheck = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Cryptography\Configuration\Local\SSL\00010002\ -Name Functions | select -ExpandProperty Functions 
         $ciphercheck2 = Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Cryptography\Configuration\Local\SSL\00010003\ -Name Functions | select -ExpandProperty Functions
         $reply = Read-Host -Prompt "Do you wish to view the Ciphers?[y/n]"
         ""
         If ( $reply -like "y" ) 
-        {write-host 
+        {Write-Output 
         $ciphercheck 
         $ciphercheck2}
-        Else{Write-Host "Skipping Cipher Check"}
+        Else{Write-Output "Skipping Cipher Check"}
         }
     function AV-Check 
-        {
+        {Write-Output "Installed Antivirus Software"  
         $ErrorActionPreference = "SilentlyContinue"
-        $antivirus = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -match "trend|mcafee|eset|symantec|norton|bitdefender|sophos|kapersky|avast|avg|avg|clamav|virus|endpoint protection|smart security|internet security" } | Select-Object -Property DisplayName | Select -ExpandProperty DisplayName
-        If ($antivirus -eq $null){Write-Output "Anvtivirus Installed: No Antivirus Found"}Else{Write-Output "Antivirus Installed: $antivirus"}}
+        $antivirus = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -match "avast|avg|bitdefender|clamav|crowdstrike|endpoint protection|eset|internet security|kapersky|mcafee|norton|smart security|sophos|symantec|trend|virus" } | Select-Object -Property DisplayName | Select -ExpandProperty DisplayName
+        If ($antivirus -eq $null)
+        {Write-Output "No Antivirus Installed"}Else{$antivirus}
+        }
     Function Patch-Check
         {
         $patchcheck = Get-HotFix | Select-Object -Property Description,HotFixID,InstalledOn | Select-Object -Last 5 | Sort-Object -Descending | Format-Table
         Write-Output "Most Recent Patches Installed:"$patchcheck
         } 
     Function Installed-Software 
-        {Write-Host "Software Check" -ForegroundColor Yellow
+        {Write-Output "Software Check"
         $ErrorActionPreference = 'silentlycontinue'
         If (Test-Path 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server') {
         $inst = (get-itemproperty 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server').InstalledInstances
@@ -137,42 +138,22 @@ Begin {
         '^15' { "Microsoft SQL Server 2019" }
         Default { "Unsupported version." }
         }
-        Write-Host ""
-        #Write-Host "Instance: $i"
-        Write-Host "Product: $product"
-        Write-Host "Edition: $edition"
-        Write-Host "Version: $version"
-        Write-Host ""
+        Write-Output ""
+        #Write-Output "Instance: $i"
+        Write-Output "Product: $product"
+        Write-Output "Edition: $edition"
+        Write-Output "Version: $version"
+        Write-Output ""
         }
         }Else{
         Write-Output "SQL Software not installed"
         }
         ""
         $iis = (Get-WindowsFeature web-server).InstallState
-        if ($iis -eq "Installed") {Write-Host "IIS is Installed"} Else {Write-Host "IIS is NOT Installed"}
-    Function Armor-Services
-        {Write-Host "Armor Subagent Service Status" -ForegroundColor Yellow
-        $services = @{}
-        $servicenames = @('AMSP','ds_agent', 'Armor-Filebeat', 'Armor-Winlogbeat', 'Bomgar', 'QualysAgent', 'PanoptaAgent')
-        Foreach ($servicename in $servicenames ) {
-        try {
-        $servicestatus = Get-Service $servicename -ErrorAction Stop | select -ExpandProperty status
-        
-        } catch {
-        $servicestatus = 'Not Installed'        
-        }
-        $services.Add($servicename , $servicestatus)
-        }
-        New-Object psobject -Property $services | Out-Default
-        #Armor-Services
-        }
-    Function Agent-Info
-        {
-        $ErrorActionPreference = 'silentlycontinue'
-        $armoragent = gsv armor-agent | select -ExpandProperty Status
-        If ($armoragent -eq $null) {Write-Host "Armor Agent Status: Not Installed"}Else{Write-Host "Armor Agent Status: $armoragent"}
-        }}
-    Function Agent-Version
+        if ($iis -eq "Installed") {Write-Output "IIS is Installed"} Else {Write-Output "IIS is NOT Installed"}
+    Function Show-Armorservices
+        { 
+        Function Agent-Version
         {
         Write-Host "Armor Agent Information" -ForegroundColor Yellow
         $agentversion = C:\.armor\opt\armor.exe --v
@@ -180,7 +161,15 @@ Begin {
         If ($agentversion -eq $null){Write-Host "Armor Agent is not installed"}Else{Write-Host "Armor Agent Version: $agentversion"}
         }
         $ErrorActionPreference = "SilentlyContinue"
-        #Function show-subagents
+        
+        Function Agent-Info
+        {
+        $ErrorActionPreference = 'silentlycontinue'
+        $armoragent = gsv armor-agent | select -ExpandProperty Status
+        If ($armoragent -eq $null) {Write-Host "Armor Agent Status: Not Installed"}Else{Write-Host "Armor Agent Status: $armoragent"}
+        
+        }
+        Function show-subagents
         {
         $output = @()
         $reg = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty
@@ -207,21 +196,48 @@ Begin {
         $output += $tmpout
         }
         $output
-        }}
+        }
+        
+        Function Armor-Services
+        {Write-Host "Armor Subagent Service Status" -ForegroundColor Yellow
+        $services = @{}
+        $servicenames = @('AMSP','ds_agent','ds_monitor','ds_notifier','ds_agent', 'Armor-Filebeat', 'Armor-Winlogbeat','QualysAgent', 'PanoptaAgent')
+        Foreach ($servicename in $servicenames ) {
+        try {
+        $servicestatus = gsv $servicename -ErrorAction Stop | select -ExpandProperty status
+        #$servicedisplay = gsv $servicename -ErrorAction Stop | select -ExpandProperty DisplayName
+        } catch {
+        $servicestatus = 'Not Installed'        
+        }
+        $services.Add($servicename , $servicestatus)
+        }
+        New-Object psobject -Property $services | Out-Default
+        }
+        $ErrorActionPreference = "SilentlyContinue"
+        }
+        
+        {
+        $showsubagents = show-subagents
+        $agentversion = Agent-Version
+        $agentinfo = Agent-Info
+        $armorservices = Armor-Services
+        
+        Write-Output "$agentinfo"
+        Write-Output "$agentversion"
+        Write-Output "$showsubagents"
+        Write-Output "$armorservices"
+        }        
+        }
 
 Process
-{
-    If ($serverinfo){server-info}
-    If ($getuptime){Get-Uptime}
-    If ($pendingreboot){PendingReboot}
-    If ($avcheck){AV-Check}
-    If ($patchcheck){Patch-Check}
-    If ($protocolcheck){Protocolcheck}
-    If ($ciphercheck){CipherCheck}
-    If ($armorservices){Agent-Info
-                        Armor-Services
-                        show-subagents}
-    #If ($showsubagents){show-subagents}
-    If ($installedsoftware){Installed-Software}
-    #If ($all){$serverinfo;$getuptime;$pendingreboot;$avcheck;$patchcheck;$installedsoftware;$protocolcheck;$ciphercheck;$armorservices}
+        {
+        If ($serverinfo){server-info}
+        If ($getuptime){Get-Uptime}
+        If ($pendingreboot){PendingReboot}
+        If ($avcheck){AV-Check}
+        If ($patchcheck){Patch-Check}
+        If ($protocolcheck){Protocolcheck}
+        If ($ciphercheck){CipherCheck}
+        If ($installedsoftware){Installed-Software}
+        If ($showarmorservices){Show-Armorservices}
 }
